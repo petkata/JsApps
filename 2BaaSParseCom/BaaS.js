c64c6a9220226b274de403b1bbe05283a7e8f99f
petkata <petyr_yontchev@abv.bg>
2014-11-26 20:04:03 +0200

    addTown issue
    The addTown button is not working properly

--- 2BaaSParseCom/BaaS.js
+++ 2BaaSParseCom/BaaS.js
@@ -1,4 +1,11 @@
 'use strict';
+/*
+Buttons have classes instead of ids to edit only one at a time, because of the location.reload()
+addTown button is not finished
+if addCountry is clicked before country is clicked, on addTown click there is a bug
+
+*/
+
 (function(){
 	$(function(){
 		var PARSE_APP_ID = "Cg1ozEUcNGVI86HhjTrDksJsiR8VfzhlvTndbd4T";
@@ -10,38 +17,48 @@
 				"X-Parse-REST-API-Key": PARSE_REST_API_KEY
 				},
 				url: "https://api.parse.com/1/classes/Country",
-				}).success(function(data) 
+				})
+		.success(function(data) 
 					{
 						for (var countries in data.results) 
 						{
 							var country = data.results[countries];
 							var countryName = $('<li>');
-							//countryName.text(country.name+" ");
+							var stringCountry = 'country';
 							var countryLink = $("<a href='#'>").text(country.name);
 							$(countryLink).data('country', country);
 							countryLink.click(countryClicked);
 							countryName.append(countryLink).prepend('<p>');
 							countryName.append("&ensp;");
-							var editBtn = $("<button class = 'editBtn'>Edit</button>");
-							editBtn.data('country', country);
-							editBtn.click(showEditField);
-							editBtn.appendTo(countryName);
-							var deleteBtn = $("<button>Delete</button>");
-							deleteBtn.data('country', country);
-							deleteBtn.click(deleteCountry);
-							deleteBtn.appendTo(countryName);
+							addButtons(country,countryName,stringCountry);									
 							countryName.appendTo($("#listCountries")).append('<p>');
 						}
-						var addBtn = $("<button id = 'addBtn'>Add Country</button>");
-					    addBtn.click(showAddField);
+						var addBtn = $("<button class = 'addBtn'>Add Country</button>");
+						addBtn.click(showAddField);
 						addBtn.appendTo($('#listCountries'));
 					}
-				).error(function() 
-					{
-						alert('Cannot load countries!');
-					});
+		)
+		.error(function() 
+			{
+				alert('Cannot load countries!');
+			}
+		);
 		
-		function countryClicked() {
+		function addButtons(element,elementName,stringParam)
+		{
+			 
+			var editBtn = $("<button class = 'editBtn'>Edit</button>");
+			editBtn.data(stringParam, element);
+			editBtn.click(showEditField);
+			editBtn.appendTo(elementName);
+			var deleteBtn = $("<button>Delete</button>");
+			deleteBtn.data(stringParam, element);
+			deleteBtn.click(deleteCountry);
+			deleteBtn.appendTo(elementName);
+		}
+		
+		function countryClicked() 
+		{
 			var country = $(this).data('country');
 			$("#towns").show();
 			var countryName = country.name;
@@ -64,19 +81,70 @@
 		
 		function townsLoaded(data)
 		{
-			$.getScript("https://raw.githubusercontent.com/petkata/JsApps/master/2BaaSParseCom/Towns.js");
 			
+			$("#towns ul").html('');
+			for (var towns in data.results) {
+				var town = data.results[towns];
+				var townName = $('<li>');
+				var stringTown = 'town';
+				var typeTown = "Town";		
+				townName.text(town.name+" ");
+				addButtons(town,townName,stringTown);
+				townName.appendTo($("#towns ul"));			
+			}
+			var addBtn = $("<button class = 'addBtn'>Add Town</button>");
+			addBtn.data.results;
+			addBtn.click(showAddField);
+			addBtn.appendTo($('#listTowns'));
 		}
 		
 		function showAddField() 
 		{
-			$('#addBtn').show();
 			
+			$('.addBtn').hide();
+			var country = $(this).data('country');
 			var addInput = $('<input type="text" class="addInput">');
 			var submit = $('<input type="submit" value="Add" class="addInput">');
-			submit.click(addCountry);
+			var cancel = $('<input type="submit" value="Cancel" class="addInput">');
+			cancel.click(function(){$('.addBtn').show();$(this).parent().remove();});
+			
+			$(this).parent().append($('<span id = "adder">').append(addInput).append(submit).append(cancel));
+			
+			var typeString="";
+			if($(this).parent().parent().attr("id")== "towns")
+			{
+				submit.click(addTown);					
+			}
+			else
+			{
+				submit.click(addCountry);
+			}
+//TOASK			 How to add the country objectId?
+			function addTown()
+					{
+					
+					var newTown = addInput.val();
+					$.ajax({
+					method: "POST",
+					headers: {
+					"X-Parse-Application-Id": PARSE_APP_ID,
+					"X-Parse-REST-API-Key": PARSE_REST_API_KEY
+					},
+					data: JSON.stringify({
+						"name": newTown,
+						"country" : {
+						"__type": "Pointer",
+						"className": "Country",
+						"objectId": country.objectId  // ???
+					}}),
+					url: "https://api.parse.com/1/classes/Town/"
+					});
+					alert(newTown + " added");	
+					location.reload();
+					}
 			function addCountry()
 			{
+				
 				var newCountry = addInput.val();
 				$.ajax({
 				method: "POST",
@@ -93,20 +161,47 @@
 				//$('#addBtn').show();
 				//$('.addInput').remove();	
 				location.reload();
-			}			
-			$('#listCountries').append(addInput).append(submit);
+			}
+			
 		}
 		
 		function showEditField() 
 		{
 			//edit only one at a time
 			$('.editBtn').hide();
-			var country = $(this).data('country');
+			var country = "";
+			var town = "";
 			var editInput = $('<input type="text" class="editInput">');
 			var submit = $('<input type="submit" value="Edit" class="editInput">');
 			var cancel = $('<input type="submit" value="Cancel" class="editInput">');
 			cancel.click(function(){$('.editBtn').show();$(this).parent().remove();});
-			submit.click(countryEditor);
+			
+			if($(this).parent().parent().attr("id")== "listTowns")
+			{		
+				submit.click(townEditor);
+				town = $(this).data('town');
+			}
+			else
+			{
+				country = $(this).data('country');
+				submit.click(countryEditor);
+			}
+			function townEditor()
+			{
+				var editTown = editInput.val();
+				$.ajax({
+				method: "PUT",
+				headers: {
+				"X-Parse-Application-Id": PARSE_APP_ID,
+				"X-Parse-REST-API-Key": PARSE_REST_API_KEY
+				},
+				data: JSON.stringify({"name":editTown}),
+				url: "https://api.parse.com/1/classes/Town/"+town.objectId
+				});
+				alert(town.name +" edited to " + editTown);	
+				location.reload();
+			}
+			
 			function countryEditor()
 			{
 				var editCountry = editInput.val();
@@ -129,17 +224,42 @@
 		
 		function deleteCountry() 
 		{
-			var country = $(this).data('country');
-			$.ajax({
-			method: "DELETE",
-			headers: {
-			"X-Parse-Application-Id": PARSE_APP_ID,
-			"X-Parse-REST-API-Key": PARSE_REST_API_KEY
-			},
-			url: "https://api.parse.com/1/classes/Country/"+country.objectId
-			});
-			location.reload();
-			alert(country.name +" deleted");
+			
+			var country = "";
+			var town = "";
+			if($(this).parent().parent().attr("id")== "listTowns")
+			{		
+				
+				town = $(this).data('town');
+				$.ajax({
+					method: "DELETE",
+					headers: {
+					"X-Parse-Application-Id": PARSE_APP_ID,
+					"X-Parse-REST-API-Key": PARSE_REST_API_KEY
+					},
+					url: "https://api.parse.com/1/classes/Town/"+town.objectId
+				});
+				location.reload();
+				alert(town.name +" deleted");
+			}
+			else
+			{
+				country = $(this).data('country');
+				
+				$.ajax({
+					method: "DELETE",
+					headers: {
+					"X-Parse-Application-Id": PARSE_APP_ID,
+					"X-Parse-REST-API-Key": PARSE_REST_API_KEY
+					},
+					url: "https://api.parse.com/1/classes/Country/"+country.objectId
+				});
+				location.reload();
+				alert(country.name +" deleted");
+			}
+			
+			
+			
 		}			
 	})
 })();
